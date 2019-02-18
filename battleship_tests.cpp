cf2b531872896cb7902414e12a4341e452e751f6
Simeon Ng <ssng@alaska.edu>
2019-02-17 15:26:20 -0900

    Add test files

---
+++ battleship_tests.cpp
@@ -0,0 +1,7 @@
+// battleship_tests.cpp
+// Simeon Ng
+// Jason Herning
+// CS372 Software Construction
+// Updated 2/17/19
+// Catch2 Tests for Battleship Game
+
---
+++ main.cpp
@@ -0,0 +1,10 @@
+// main.cpp
+// Simeon Ng
+// Jason Herning
+// CS372 Software Construction
+// Updated 2/17/19
+// main() for Battleship Game
+
+#define CATCH_CONFIG_MAIN // Tell catch to provide a main()
+#include "catch.hpp"
+
