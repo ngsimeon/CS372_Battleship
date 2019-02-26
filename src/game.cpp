fcea68157dae511710b34cdc0bf81a5fad50da78
Simeon Ng <ssng@alaska.edu>
2019-02-26 12:26:24 -0900

    Add gameLoop outline

--- src/game.cpp
+++ src/game.cpp
@@ -13,5 +13,17 @@ Game::Game() = default;
 // ---- Game : Public Member Functions
 
 void Game::gameLoop() {
+    // Player 1 places ships
+    // Player 2 places ships
 
+    // Game loop starts
+    // - Alternating fire by p1 and p2
+    // ----> Print p1 board
+    // ----> p1 fires.
+    // ----> Hide p1 board
+    // ----> Print p2 board
+    // ----> p2 fires.
+    // ----> Hide p2 board
+
+    // - check if all ships are sunken.
 }
